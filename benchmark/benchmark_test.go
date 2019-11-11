41e5296e7b335aac2079ffa86bedcf8370d2a086
Roman Sharkov <roman.scharkov@gmail.com>
2019-11-11 18:36:35 +0100

    Improve benchmark test
    Make sure Setup function signatures are correct.

--- benchmark/benchmark_test.go
+++ benchmark/benchmark_test.go
@@ -1,6 +1,7 @@
 package benchmark_test
 
 import (
+	"fmt"
 	"logbench/benchmark"
 	"reflect"
 	"testing"
@@ -17,6 +18,54 @@ func TestSetupFunctionTypes(t *testing.T) {
 			reflect.Func,
 			fl.Type.Kind(),
 			"Setup.%s type is not a function",
+			fl.Name,
+		)
+
+		require.Equal(
+			t,
+			1,
+			fl.Type.NumIn(),
+			"Setup.%s must accept io.ReadWriter as its first argument",
+			fl.Name,
+		)
+
+		require.Equal(
+			t,
+			1,
+			fl.Type.NumIn(),
+			"Setup.%s must accept io.ReadWriter as its first argument",
+			fl.Name,
+		)
+
+		in1 := fl.Type.In(0)
+		require.True(
+			t,
+			fmt.Sprintf("%s.%s", in1.PkgPath(), in1.Name()) == "io.ReadWriter",
+			"Setup.%s must accept io.ReadWriter as its first argument",
+			fl.Name,
+		)
+
+		require.Equal(
+			t,
+			2,
+			fl.Type.NumOut(),
+			"Setup.%s must return (func(...), error)",
+			fl.Name,
+		)
+
+		out1 := fl.Type.Out(0)
+		require.True(
+			t,
+			reflect.Func == out1.Kind() && out1.NumOut() == 0,
+			"Setup.%s must return (func(...), error)",
+			fl.Name,
+		)
+
+		out2 := fl.Type.Out(1)
+		require.True(
+			t,
+			out2.Kind() == reflect.Interface && out2.Name() == "error",
+			"Setup.%s must return (func(...), error)",
 			fl.Name,
 		)
 	}
